aca71658c2da8fa2faf494a7cf22e253fd5f958d
Jae hyuk Shim <jaehyukshim@Jaeui-MacBookPro.local>
2025-06-29 00:46:40 +0900

    처음 커밋✨

--- Test.sh
+++ Test.sh
@@ -4,4 +4,5 @@ git add .
 git commit -m "처음 커밋✨"
 git branch -M main
 git remote add origin https://github.com/HyangDan2/GHPilot0001
-git push -u origin main
+git pull origin main --rebase
+git push origin main
